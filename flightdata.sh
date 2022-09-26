4a2a0f22d1988ffa3fb3b0ef2b7389a74dee2ecd
Morgan Masters <mwmaster@ucsc.edu>
2022-09-26 15:57:00 -0700

    ayy bruh edits to flightdata

--- flightdata.sh
+++ flightdata.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
+# the place to save pids for killing 
+# FILE=pids.txt
 # saving the data to a text file
 FILE=fdata.txt
 
-# clearing the file and making a new one
-rm fdata.txt; touch fdata.txt
+# restart nvargus to make it extra happy
+sudo systemctl restart nvargus-daemon
 
-# launching the vehcile node to the background and save data to the flight data file 
+# roscore & echo $! >> $FILE
+# clear  pids
+# rm pids.txt; touch pids.txt
+rm $FILE; touch $FILE
+
+
+# roscore
+roscore & echo $! >> $FILE
+sleep 10
+# launch the cameras to background and save the pid to pids.txt
+roslaunch ~/catkin_ws/src/img_pipeline/launch/nv12_test.launch & echo $! >> $FILE
+# launching the vehicle node to the background and save data to the flight data file 
 roslaunch ~/catkin_ws/src/Onboard-SDK-ROS/launch/dji_vehicle_node.launch & echo $! >> $FILE
-#cd ~/catkin_ws/src/Onboard-SDK-ROS/launch
-#roslaunch dji_vehicle_node.launch & echo $! >> $FILE
+# launch the gps node
+roslaunch ~/catkin_ws/src/ros_zedf9p/ublox_gps/launch/hare_gps.launch & echo $! >> $FILE
+# launch micasense node
+# python3 ~/catkin_ws/src/micasense/scripts/main_multi.py & echo $! >> $FILE
 
 
-# recording the topics
-rosbag record /dji_osdk_ros/attitude /dji_osdk_ros/battery_state /dji_osdk_ros/flight_status /dji_osdk_ros/height_above_takeoff /dji_osdk_ros/imu /dji_osdk_ros/gps_position /dji_osdk_ros/local_position /dji_osdk_ros/rc /dji_osdk_ros/rc_connection_status /dji_osdk_ros/velocity & echo $! >> $FILE
+# launch imu
+# rosrun rosserial_arduino serial_node.py _port:=/dev/ttyACM0 _baud:=115200 & echo $! >> $FILE
 
+# launch the rosbag to background and save the pid to pids.txt
+#rosbag record /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /imu & echo $! >> $FILE
+
+#rosbag record -o /media/jetson/hareflightdisk00/ /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /dji_osdk_ros/attitude /dji_osdk_ros/battery_state /dji_osdk_ros/flight_status /dji_osdk_ros/height_above_takeoff /dji_osdk_ros/imu /dji_osdk_ros/gps_position /dji_osdk_ros/local_position /dji_osdk_ros/rc /dji_osdk_ros/rc_connection_status /dji_osdk_ros/velocity /ublox/fix & echo $! >> $FILE
+
+rosbag record $(cat djiRosTopics.json | jq -r '.active_topics | .[]') /media/jetson/hareflightbag011/ /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /ublox/fix & echo $! >> $FILE
+
+
+# dji topics
 # acceleration ground fused
 #rosbag record /dji_osdk_ros/acceleration_ground_fused & echo $! >> $FILE
 # angular velocity 
