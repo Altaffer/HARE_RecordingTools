fe0e094ee3ea22c893d3d4ac1732ef2f4ee00d43
Morgan Masters <mwmaster@ucsc.edu>
2022-10-12 15:45:19 -0700

    Adding additions from recording tools

---
+++ README.md
@@ -0,0 +1,26 @@
+Record data:
+./flightdata.sh
+
+Run just the cameras:
+./flightdata.sh cameras
+
+
+TMUX hax
+- Initiate actions: Ctrl + b = C-b
+- Create a new window: C-b c
+- Split window horizontally: C-b " (shift ')
+- Split vertically: C-b % (shift 5)
+- Navigate between windows: C-b n / C-b p / C-b <number> 
+- Navigate between panes (split window): C-b <arrow>
+- Close pane: C-d
+- Detach session: C-b d
+- List backgrounded session: tmux ls
+- Attach session: tmux attach -t 0 / tmux attach -t recording
+- Naming sussions: tmux new -s recording / tmux rename-session -t 0 recording
+- Make pane full screen (and back from full screen): C-b z
+- Resize pane: C-b C-<arrow>
+- Rename current window: C-b ,
+- Quality of life configs
+	- Modify ~/.tmux.conf
+		set -g mode-mouse on
+		set -g default-terminal "xterm-256color"
--- flightdata.sh
+++ flightdata.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $#
+
 # the place to save pids for killing 
 # FILE=pids.txt
 # saving the data to a text file
@@ -20,11 +22,11 @@ sleep 10
 # launch the cameras to background and save the pid to pids.txt
 roslaunch ~/catkin_ws/src/img_pipeline/launch/nv12_test.launch & echo $! >> $FILE
 # launching the vehicle node to the background and save data to the flight data file 
-roslaunch ~/catkin_ws/src/Onboard-SDK-ROS/launch/dji_vehicle_node.launch & echo $! >> $FILE
+# roslaunch ~/catkin_ws/src/Onboard-SDK-ROS/launch/dji_vehicle_node.launch & echo $! >> $FILE
 # launch the gps node
 roslaunch ~/catkin_ws/src/ros_zedf9p/ublox_gps/launch/hare_gps.launch & echo $! >> $FILE
 # launch micasense node
-# python3 ~/catkin_ws/src/micasense/scripts/main_multi.py & echo $! >> $FILE
+python3 ~/catkin_ws/src/micasense/scripts/main.py & echo $! >> $FILE
 
 
 # launch imu
@@ -34,9 +36,9 @@ roslaunch ~/catkin_ws/src/ros_zedf9p/ublox_gps/launch/hare_gps.launch & echo $! 
 #rosbag record /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /imu & echo $! >> $FILE
 
 #rosbag record -o /media/jetson/hareflightdisk00/ /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /dji_osdk_ros/attitude /dji_osdk_ros/battery_state /dji_osdk_ros/flight_status /dji_osdk_ros/height_above_takeoff /dji_osdk_ros/imu /dji_osdk_ros/gps_position /dji_osdk_ros/local_position /dji_osdk_ros/rc /dji_osdk_ros/rc_connection_status /dji_osdk_ros/velocity /ublox/fix & echo $! >> $FILE
-
-rosbag record $(cat djiRosTopics.json | jq -r '.active_topics | .[]') /media/jetson/hareflightbag011/ /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /ublox/fix & echo $! >> $FILE
-
+# if [[$# == 0 ]]; then
+rosbag record $(cat djiRosTopics.json | jq -r '.active_topics | .[]') -o /media/jetson/hareflightdisk001/ /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /ublox/fix /micasense_data & echo $! >> $FILE
+#fi
 
 # dji topics
 # acceleration ground fused
