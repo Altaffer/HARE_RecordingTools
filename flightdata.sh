cb71897e1bbd85f885b135c003d69d8ea65f3619
Morgan Masters <mwmaster@ucsc.edu>
2022-10-13 14:57:43 -0700

    fixed stupid

--- flightdata.sh
+++ flightdata.sh
@@ -23,30 +23,11 @@ sleep 10
 # launch the cameras to background and save the pid to pids.txt
 roslaunch ~/catkin_ws/src/img_pipeline/launch/nv12_test.launch & echo $! >> $FILE
 # launching the vehicle node to the background and save data to the flight data file 
-# roslaunch ~/catkin_ws/src/Onboard-SDK-ROS/launch/dji_vehicle_node.launch & echo $! >> $FILE
-# launch the gps node
-roslaunch ~/catkin_ws/src/ros_zedf9p/ublox_gps/launch/hare_gps.launch & echo $! >> $FILE
-# launch micasense node
-python3 ~/catkin_ws/src/micasense/scripts/main.py & echo $! >> $FILE
-
-
-# roscore & echo $! >> $FILE
-# clear  pids
-# rm pids.txt; touch pids.txt
-rm $FILE; touch $FILE
-
-
-# roscore
-roscore & echo $! >> $FILE
-sleep 10
-# launch the cameras to background and save the pid to pids.txt
-roslaunch ~/catkin_ws/src/img_pipeline/launch/nv12_test.launch & echo $! >> $FILE
-# launching the vehicle node to the background and save data to the flight data file 
 roslaunch ~/catkin_ws/src/Onboard-SDK-ROS/launch/dji_vehicle_node.launch & echo $! >> $FILE
 # launch the gps node
 roslaunch ~/catkin_ws/src/ros_zedf9p/ublox_gps/launch/hare_gps.launch & echo $! >> $FILE
 # launch micasense node
-# python3 ~/catkin_ws/src/micasense/scripts/main_multi.py & echo $! >> $FILE
+# python3 ~/catkin_ws/src/micasense/scripts/main.py & echo $! >> $FILE
 
 
 # launch imu
@@ -57,5 +38,5 @@ roslaunch ~/catkin_ws/src/ros_zedf9p/ublox_gps/launch/hare_gps.launch & echo $! 
 
 #rosbag record -o /media/jetson/hareflightdisk00/ /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /dji_osdk_ros/attitude /dji_osdk_ros/battery_state /dji_osdk_ros/flight_status /dji_osdk_ros/height_above_takeoff /dji_osdk_ros/imu /dji_osdk_ros/gps_position /dji_osdk_ros/local_position /dji_osdk_ros/rc /dji_osdk_ros/rc_connection_status /dji_osdk_ros/velocity /ublox/fix & echo $! >> $FILE
 # if [[$# == 0 ]]; then
-rosbag record $(cat djiRosTopics.json | jq -r '.active_topics | .[]') -o /media/jetson/hareflightdisk001/ /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /ublox/fix /micasense_data & echo $! >> $FILE
+# rosbag record $(cat djiRosTopics.json | jq -r '.active_topics | .[]') -o /media/jetson/hareflightdisk001/ /cam0/camera/image_raw /cam1/camera/image_raw /therm/image_raw_throttle /ublox/fix /micasense_data & echo $! >> $FILE
 #fi
